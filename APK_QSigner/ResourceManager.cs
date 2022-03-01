aee49de172d787feca2df404402ed95c061cd255
j4ck <cacodemon1123@gmail.com>
2022-03-01 16:30:14 +0200

    More sorting...

--- APK_QSigner/ResourceManager.cs
+++ APK_QSigner/ResourceManager.cs
@@ -3,6 +3,9 @@ using APK_QSigner.Properties;
 
 namespace APK_QSigner
 {
+    /// <summary>
+    /// A little class to help with the resources of the app.
+    /// </summary>
     public class ResourceManager
     {
         public static Bitmap GetImageFromResources(string resource)
--- APK_QSigner/SignForm.cs
+++ APK_QSigner/SignForm.cs
@@ -73,7 +73,5 @@ namespace APK_QSigner
             }
         }
         #endregion
-
-        
     }
 }
